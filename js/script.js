54dc7c9e385737d6edf9f1d9a87d484ddb21c9de
pomelodouble <547897420@qq.com>
2019-02-03 23:42:30 +0800

    Fix Scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
  $(document).ready(function() {
     // Init Scrollspy
     $('body').scrollspy({
-        target:'#main-nav'
+        target:'#main-nav',
+        offset: 50
     });
 
     // Smooth Scrolling
@@ -12,10 +13,9 @@
         const hash = this.hash;
 
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            scrollTop: $(hash).offset().top - 50
         }, 800, function () {
-
-            window.location.hash = hash;
+            // window.location.hash = hash;
         });
         }
     }); 
